c3895b2fb22206434c3830f13795729ee6c04259
Ahmad Salah <ahmed.s@makla.ca>
2026-01-19 21:33:35 +0200

    view categories

--- src/app/[locale]/(admin)/dashboard/categories/page.tsx
+++ src/app/[locale]/(admin)/dashboard/categories/page.tsx
@@ -1,89 +1,91 @@
-import { createClient } from "@/lib/supabase/server";
-import { Metadata } from "next";
-import Pagination from "../../components/tables/Pagination";
-import { PostgrestError } from "@supabase/supabase-js";
-import _ from "lodash"
-import { CategoryType } from "@/types/CategoryType";
-import CategoriesTable from "./components/CategoriesTable";
-import UpdateToastHandler from "../../components/common/UpdateToastHandler";
+"use client";
+import _ from "lodash";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useEffect } from "react";
 import SearchInput from "../../components/common/SearchInput";
+import UpdateToastHandler from "../../components/common/UpdateToastHandler";
+import Pagination from "../../components/tables/Pagination";
+import { useSearchParams } from "next/navigation";
+import CategoriesTable from "./components/CategoriesTable";
+import getCategories from "@/lib/actions/categories/getCategories";
+import CategoriesTableSkeleton from "./components/CategoriesTableSkeleton";
 
-export const metadata: Metadata = {
-  title: "Categories",
-  description:
-    "This is Next.js Basic Table  page for TailAdmin  Tailwind CSS Admin Dashboard Template",
-  // other metadata
+const PAGE_SIZE = 10
+
+type CategoriesParamsType = {
+  page?: string,
+  q?: string
 };
 
-const PAGE_SIZE = 10;
+function CategoriesPage() {
+  const searchParams = useSearchParams();
+  const categoriesParams: CategoriesParamsType = Object.fromEntries(searchParams.entries());
 
-type Props = {
-  searchParams: Promise<{
-    page?: string;
-    q?: string;
-  }>;
-};
+  const { data, isLoading, isFetching, error } = useQuery({
+    queryKey: ["categories", categoriesParams.page, categoriesParams.q],
+    queryFn: () => getCategories({...categoriesParams, PAGE_SIZE}),
+  });
 
-export default async function CategoriesPage({ searchParams }: Props) {
-  const supabase = await createClient();
-  const { page: paramsPage, q = "" } = await searchParams;
+  useEffect(() => {
+    if (isFetching) {
+      toast("Refreshing...");
+    }
+  }, [isFetching]);
 
-  const page = Math.max(Number(paramsPage) || 1, 1);
-  const from = (page - 1) * PAGE_SIZE;
-  const to = from + PAGE_SIZE - 1;
+  if (isLoading)
+    return (
+      <div className="space-y-6">
+        <div className="flex flex-wrap items-center justify-between gap-8 mb-6 w-full">
+          <h2 className="text-xl font-semibold text-gray-800 dark:text-white/90">
+            Categories
+          </h2>
+          <SearchInput placeholder="Search categories..." />
+        </div>
+        <CategoriesTableSkeleton />
+      </div>
+    );
 
-  /** 1. Fetch categories chained with search value */
-  let query = supabase
-    .from("categories")
-    .select("*", {
-      count: "exact",
-    })
-    .is("deleted_at", null)
-    .order("created_at", { ascending: false })
-    .range(from, to);
-
-  if (q) {
-    query = query.or(`name.ilike.%${q}%`);
-  }
-
-  /** 2. Await for categories */
-  const {
-    data: categories,
-    error,
-    count,
-  }: {
-    data: Array<CategoryType> | null;
-    error: PostgrestError | null;
-    count: number | null;
-  } = await query;
-
-  if (error) {
-    console.error(error);
-    return <p className="text-red-500">Failed to load categories</p>;
-  }
-
-  const totalPages = Math.ceil((count ?? 0) / PAGE_SIZE);
+  if (error) return <p className="text-red-500">Failed to load categories</p>;
 
   return (
     <div>
-      <UpdateToastHandler message="Category updated successfully!" urlToReplace="/dashboard/categories"/>
+      <UpdateToastHandler
+        message="Category updated successfully!"
+        urlToReplace="/dashboard/categories"
+      />
       <div className="flex flex-wrap items-center justify-between gap-8 mb-6 w-full">
         <h2 className="text-xl font-semibold text-gray-800 dark:text-white/90">
           Categories
         </h2>
-        <SearchInput placeholder="Search categories..."/>
+        <SearchInput placeholder="Search categories..." />
       </div>
-      {_.isEmpty(categories) && !q && <p className="text-center text-gray-500 dark:text-gray-400 mt-16">There are no categories</p>}
-      {_.isEmpty(categories) && q && <p className="text-center text-gray-500 dark:text-gray-400 mt-16">There is no category {q && `matches "${q}"`}</p>}
-      {!_.isEmpty(categories) && <div className="space-y-6">
-        <CategoriesTable categories={categories ?? []} />
-        <div className="flex justify-between items-center">
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            Showing {PAGE_SIZE} entries
-          </p>
-          <Pagination currentPage={page} totalPages={totalPages} />
+      {_.isEmpty(data?.categories) && !categoriesParams.q && (
+        <p className="text-center text-gray-500 dark:text-gray-400 mt-16">
+          There are no categories
+        </p>
+      )}
+      {_.isEmpty(data?.categories) && categoriesParams.q && (
+        <p className="text-center text-gray-500 dark:text-gray-400 mt-16">
+          There is no category {categoriesParams.q && `matches "${categoriesParams.q}"`}
+        </p>
+      )}
+      {!_.isEmpty(data?.categories) && (
+        <div className="space-y-6">
+          <CategoriesTable categories={data?.categories ?? []} />
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              Showing {PAGE_SIZE} entries
+            </p>
+            <Pagination
+              currentPage={data?.page as number}
+              totalPages={data?.totalPages as number}
+            />
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 }
+
+export default CategoriesPage;
---
+++ src/lib/actions/categories/getCategories.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { CategoryType } from "@/types/CategoryType";
+import { PostgrestError } from "@supabase/supabase-js";
+
+type Props = {
+  page?: string;
+  q?: string;
+  PAGE_SIZE: number
+};
+
+async function getCategories({ page: paramsPage, q, PAGE_SIZE }: Props) {
+  const supabase = await createClient();
+
+  const page = Math.max(Number(paramsPage) || 1, 1);
+  const from = (page - 1) * PAGE_SIZE;
+  const to = from + PAGE_SIZE - 1;
+
+  /** 1. Fetch categories chained with search value */
+  let query = supabase
+    .from("categories")
+    .select("*", {
+      count: "exact",
+    })
+    .order("created_at", { ascending: false })
+    .range(from, to);
+
+  if (q) {
+    query = query.or(`name.ilike.%${q}%`);
+  }
+
+  /** 2. Await for categories */
+  const {
+    data: categories,
+    error,
+    count,
+  }: {
+    data: Array<CategoryType> | null;
+    error: PostgrestError | null;
+    count: number | null;
+  } = await query;
+
+  if (error) throw error;
+
+  const totalPages = Math.ceil((count ?? 0) / PAGE_SIZE);
+
+  return { categories, page, totalPages };
+}
+
+export default getCategories;
