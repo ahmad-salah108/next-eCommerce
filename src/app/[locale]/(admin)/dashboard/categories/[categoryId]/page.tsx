7ffd14a95855fa5691776f49b993ca6fee4927ce
Ahmad Salah <ahmed.s@makla.ca>
2026-02-02 15:35:22 +0200

    handled user navigation to [id] without [slug]

--- src/app/[locale]/(admin)/dashboard/categories/[categoryId]/page.tsx
+++ src/app/[locale]/(admin)/dashboard/categories/[categoryId]/page.tsx
@@ -1,7 +1,25 @@
-import { notFound } from 'next/navigation'
+"use client";
+import { getCategoryById } from "@/lib/actions/categories/getCategoryById";
+import { useQuery } from "@tanstack/react-query";
+import { notFound, redirect } from "next/navigation";
+import { use, useEffect } from "react";
 
-function CategoryIdPage() {
-  return notFound()
+function CategoryIdPage({ params }: { params: Promise<{ categoryId: string }> }) {
+  const { categoryId } = use(params);
+  const { data, error } = useQuery({
+    queryKey: ["categories", categoryId],
+    queryFn: () => getCategoryById(categoryId),
+  });
+
+  if (error) notFound();
+
+  useEffect(() => {
+    if (data?.slug) redirect(`/dashboard/categories/${categoryId}/${data?.slug}`);
+  }, [data?.slug, categoryId]);
+
+  return (
+    <h1 className="dark:text-white/90">Loading...</h1>
+  )
 }
 
-export default CategoryIdPage
+export default CategoryIdPage;
--- src/app/[locale]/(admin)/dashboard/products/[productId]/page.tsx
+++ src/app/[locale]/(admin)/dashboard/products/[productId]/page.tsx
@@ -1,7 +1,25 @@
-import { notFound } from 'next/navigation'
+"use client";
+import { getProductById } from "@/lib/actions/products/getProductById";
+import { useQuery } from "@tanstack/react-query";
+import { notFound, redirect } from "next/navigation";
+import { use, useEffect } from "react";
 
-function ProductIdPage() {
-  return notFound()
+function ProductIdPage({ params }: { params: Promise<{ productId: string }> }) {
+  const { productId } = use(params);
+  const { data, error } = useQuery({
+    queryKey: ["products", productId],
+    queryFn: () => getProductById(productId),
+  });
+
+  if (error) notFound();
+
+  useEffect(() => {
+    if (data?.slug) redirect(`/dashboard/products/${productId}/${data?.slug}`);
+  }, [data?.slug, productId]);
+
+  return (
+    <h1 className="dark:text-white/90">Loading...</h1>
+  )
 }
 
-export default ProductIdPage
+export default ProductIdPage;
